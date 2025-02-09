d42b084abb03dbe4053fba76a5e778e3ceb523ef
Tomas Jansson <tomas.jansson@gmail.com>
2025-02-09 23:22:48 +0100

    Me slide done

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { CldVideoPlayer } from "next-cloudinary";
-import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
 import "../node_modules/next-cloudinary/dist/cld-video-player.css";
 
 export type VideoPlayerProps = {
@@ -19,25 +17,9 @@ export const VideoPlayer = ({
   cloudinaryId,
   className,
 }: VideoPlayerProps) => {
-  const [key, setKey] = useState<string | undefined>();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const playerId = id ?? cloudinaryId;
   const posterUrl = `https://res.cloudinary.com/dfsp4lpf1/video/upload/so_0/c_limit,h_700,w_400/${cloudinaryId}.avif`;
-  useEffect(() => {
-    const uniqueValue = Math.random() * 1000;
-    const newKey = `${playerId}-${pathname}-${
-      searchParams.get("slide") ?? "0"
-    }-${uniqueValue}`;
-    setKey(newKey);
-  }, [pathname, playerId, searchParams]);
-  if (!key) {
-    return null;
-  }
-  console.log("==> Player key: ", key);
   return (
     <CldVideoPlayer
-      key={key}
       width={width}
       height={height}
       src={cloudinaryId}
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,6 @@
 import type { MDXComponents } from "mdx/types";
 import * as motion from "motion/react-client";
+import { PropsWithChildren } from "react";
 
 export const CustomH2 = ({ children }: { children: React.ReactNode }) => (
   <motion.h1
@@ -38,9 +39,23 @@ export const CustomBlockquote = ({
   </motion.blockquote>
 );
 
+export const CustomUL = ({ children }: PropsWithChildren) => {
+  return (
+    <motion.ul
+      initial={{ opacity: 0, x: -20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5, delay: 0.2 }}
+      className="flex flex-col gap-2 pl-6 my-6 text-lg md:text-xl list-disc"
+    >
+      {children}
+    </motion.ul>
+  );
+};
+
 export const mdxComponents = {
   h1: CustomH1,
   h2: CustomH2,
+  ul: CustomUL,
   blockquote: CustomBlockquote,
 };
 
