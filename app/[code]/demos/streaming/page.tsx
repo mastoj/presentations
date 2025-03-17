84427c3e66536ea2c103abc313bdf562609c9773
Tomas Jansson <tomas.jansson@gmail.com>
2025-03-17 01:16:04 +0100

    Streaming vs cached content

--- app/[code]/demos/streaming/page.tsx
+++ app/[code]/demos/streaming/page.tsx
@@ -1,5 +1,6 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
+import { unstable_cache as cache } from "next/cache";
 import { PropsWithChildren, Suspense } from "react";
 
 const Box = async ({
@@ -9,7 +10,7 @@ const Box = async ({
   return (
     <div
       className={cn(
-        "w-full h-20 flex justify-center items-center gap-2 py-8",
+        "w-full h-20 flex justify-center items-center gap-2 py-8 px-4",
         className
       )}
     >
@@ -24,19 +25,45 @@ const Content = async () => {
   return <Box>Hello streamed content!</Box>;
 };
 
+const CachedContent = async () => {
+  // Sleep for 2 seconds
+  const text = await cache(
+    async () =>
+      (await new Promise((resolve) =>
+        setTimeout(() => resolve(new Date().toUTCString()), 2000)
+      )) as string,
+    [],
+    {
+      tags: ["cached-content"],
+      revalidate: 10,
+    }
+  )();
+  return (
+    <Box className="flex-col">
+      Hello cached content!
+      <div>{text}</div>
+    </Box>
+  );
+};
+
 const ContentSkeleton = () => {
   return (
     <Box className="py-0">
-      <Skeleton className="w-full h-full" />
+      <Skeleton className="w-full h-full flex justify-center items-center">
+        Loading...
+      </Skeleton>
     </Box>
   );
 };
 
 const StaticStreamingPage = async () => {
   return (
-    <div className="h-full w-full *:border *:border-blue-700 py-8 gap-2">
+    <div className="h-full w-full flex *:border *:border-blue-700 py-8 gap-2">
       <Suspense fallback={<ContentSkeleton />}>
         <Content />
+      </Suspense>
+      <Suspense fallback={<ContentSkeleton />}>
+        <CachedContent />
       </Suspense>
     </div>
   );
