11e606598da85f7312fc9721d66671d419066c77
Tomas Jansson <tomas.jansson@gmail.com>
2025-03-17 01:00:54 +0100

    Simpler streaming demo

--- app/[code]/demos/streaming/page.tsx
+++ app/[code]/demos/streaming/page.tsx
@@ -1,34 +1,42 @@
-import { Suspense } from "react";
-import FadeInComponent from "../layout/_components/fade-in-component";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+import { PropsWithChildren, Suspense } from "react";
 
-type Props = {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-};
-
-const QueryParamComponent = async ({ searchParams }: Props) => {
-  // Sleep for 2 seconds
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+const Box = async ({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string }>) => {
   return (
-    <FadeInComponent>
-      <div>Hello query! {JSON.stringify(await searchParams)}</div>
-    </FadeInComponent>
+    <div
+      className={cn(
+        "w-full h-20 flex justify-center items-center gap-2 py-8",
+        className
+      )}
+    >
+      {children}
+    </div>
   );
 };
 
 const Content = async () => {
   // Sleep for 2 seconds
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  return <div>Hello static!</div>;
+  return <Box>Hello streamed content!</Box>;
 };
-const StaticStreamingPage = async ({ searchParams }: Props) => {
+
+const ContentSkeleton = () => {
   return (
-    <div className="h-full grid grid-flow-row justify-between *:border *:border-blue-700 py-8 gap-2">
-      StaticStreamingPage:
-      <Suspense>
+    <Box className="py-0">
+      <Skeleton className="w-full h-full" />
+    </Box>
+  );
+};
+
+const StaticStreamingPage = async () => {
+  return (
+    <div className="h-full w-full *:border *:border-blue-700 py-8 gap-2">
+      <Suspense fallback={<ContentSkeleton />}>
         <Content />
-      </Suspense>
-      <Suspense fallback={<div>Loading some stuff...</div>}>
-        <QueryParamComponent searchParams={searchParams} />
       </Suspense>
     </div>
   );
