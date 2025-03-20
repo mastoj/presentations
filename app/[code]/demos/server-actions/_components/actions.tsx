7ed462fdf358a7ee1d876fcf42f49ffbd10f3267
Tomas Jansson <tomas.jansson@gmail.com>
2025-03-20 10:28:26 +0100

    Move commen

--- app/[code]/demos/server-actions/_components/actions.tsx
+++ app/[code]/demos/server-actions/_components/actions.tsx
@@ -3,13 +3,13 @@
 export const handleFormAction = async (formData: FormData) => {
   const name = formData.get("name");
   console.log("==> Form submitted with name: ", name);
-  // Sleep 3 seconds
   if (!name || name === "" || name === "error") {
     return {
       success: false,
       message: "Name cannot be empty or 'error'",
     };
   }
+  // Sleep 3 seconds
   await new Promise((resolve) => setTimeout(resolve, 3000));
   return {
     success: true,
