9308e06e305aca629ee45dae87b541136609cff6
Tomas Jansson <tomas.jansson@gmail.com>
2025-03-30 09:19:55 +0200

    Start of refill 2025 preparations

---
+++ app/[code]/refill/2025/page.tsx
@@ -0,0 +1,12 @@
+import { ReactParisSlides } from "./slides";
+
+type SlideProps = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+const RefillPage = async ({ searchParams }: SlideProps) => {
+  const { slide } = await searchParams;
+  const slideNumber = slide ? parseInt(slide as string, 10) : 0;
+  return <ReactParisSlides initialSlide={slideNumber} />;
+};
+
+export default RefillPage;
