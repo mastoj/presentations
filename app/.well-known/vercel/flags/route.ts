3c7b7784d04f51887404c39297cfbfbd01788fea
Tomas Jansson <tomas.jansson@gmail.com>
2025-09-09 20:02:37 +0200

    Cleanup

--- app/.well-known/vercel/flags/route.ts
+++ app/.well-known/vercel/flags/route.ts
@@ -2,35 +2,6 @@ import { createFlagsDiscoveryEndpoint, getProviderData } from "flags/next";
 import * as flags from "@/lib/flags";
 
 export const GET = createFlagsDiscoveryEndpoint(async () => {
-  // your previous logic in here to gather your feature flags
   const apiData = await getProviderData(flags);
-
-  // return the ApiData directly, without a NextResponse.json object.
   return apiData;
 });
-
-// export async function GET(request: NextRequest) {
-//   const flags = await getFlags();
-//   const access = await verifyAccess(request.headers.get("Authorization"));
-//   if (!access) return NextResponse.json(null, { status: 401 });
-
-//   const featureFlags = Object.keys(flags).reduce(
-//     (acc, key) => ({
-//       ...acc,
-//       [key]: {
-//         options: [
-//           { value: false, label: "Off" },
-//           { value: true, label: "On" },
-//         ],
-//       },
-//     }),
-//     {}
-//   );
-//   const response = {
-//     definitions: {
-//       ...featureFlags,
-//     },
-//   };
-
-//   return NextResponse.json<ApiData>(response);
-// }
